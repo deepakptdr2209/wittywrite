c88348200e04836e69282ce3f33e7474fc610b14
devdeepak <deepakptdr2209@gmail.com>
2024-09-28 04:00:43 +0530

    all endpoint tested succesfully

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,27 +10,7 @@ const app = new Hono<{
     JWT_SECRET : string
   }
 }>();
-
-// !-----------Middleware --------!
-app.use('/api/v1/blog/*',async (c,next)=>{
-  const header = c.req.header("Authorization") || "" ;
-  // in jwt we get (bearer,token) but we need only token
-  const token = header.split(" ")[1];
-  //verify the token 
-  const response = await verify(token,c.env.JWT_SECRET);
-  if(response.id){
-    next();
-  }
-  else{
-    c.status(403)
-    return c.json({Error:"unathorised"})
-  }
-})
-
 app.route('/api/v1/user',userRouter);
 app.route('/api/v1/blog',blogRouter);
-
-
-
 
 export default app
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,20 +1,127 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
 
 export const blogRouter = new Hono<{
     Bindings :{
     DATABASE_URL : string,
     JWT_SECRET : string
-    }
+    },
+    Variables : {
+        userId : string 
+        }
 }>();
+  //!---------middleware -------!
+ blogRouter.use('/*',async(c,next)=>{
+    //get the header
+    //verify the user jwt token
+    //pass the result
+    const authHeader = c.req.header("Authorization")|| "";
+    const user = await verify(authHeader,c.env.JWT_SECRET);
+    if(user){
+        c.set("userId",user.id as string);
+        await next();
+    } else{
+        c.status(404);
+        return c.json({
+            message: "not signed in"
+        })
+    }
+   
+ })
+// !----------- TO ADD THE BLOG ---------!
+  blogRouter.post('/', async (c) => {
+    const body = await c.req.json();
+    const authorId = c.get("userId")
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
 
+   try {
+    const blog =  await prisma.post.create({
+        data :{
+            title : body.title,
+            content : body.content,
+            authorId : authorId
+        }
+        })
+        return c.json({
+            id : blog.id
+   })
+      }
+      catch (error) {
+      c.status(403);
+      return c.json({meassage: "access denied"});
+   }
+});
+  // !----------- TO UPDATE THE BLOG ---------!
+  blogRouter.put('/', async(c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
 
-  blogRouter.post('/', (c) => {
-    return c.text('Hello Hono!')
+   try {
+    const blog =  await prisma.post.update({
+        where:{
+            id : body.id
+        },
+        data :{
+            title : body.title,
+            content : body.content,
+            
+        }
+        })
+        return c.json({
+            id : blog.id
+   })
+      }
+      catch (error) {
+      c.status(403);
+      return c.json({meassage: "access denied"});
+   }
   })
-  blogRouter.put('/', (c) => {
-    return c.text('Hello Hono!')
-  })
-  blogRouter.get('/api/v1/blog/:id', (c) => {
-    return c.text('Hello Hono!')
+   // !----------- TO GET ALL THE BLOG ---------!
+   blogRouter.get("/bulk",async (c)=>{
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+    try {
+        const blogs = await prisma.post.findMany();
+        return c.json({
+            blogs
+        })
+    } catch (error) {
+        c.status(404)
+        return c.json({
+            message: "no blog found"
+        })
+    }
+   })
+
+  // !----------- TO GET THE BLOG ----------!
+  blogRouter.get('/:id', async (c) => {
+    const id = c.req.param("id");
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+   try {
+    const blog =  await prisma.post.findFirst({
+        where:{
+            id : id
+        },
+        
+        })
+        return c.json({
+             blog
+        })
+       }
+    catch (error) {
+      c.status(404);
+      return c.json({meassage: "OOps nothing found"});
+       }
   })
   
